06eb4ced96cea9d00301d345252248aa7140a089
Mehmet Furkan Karahan <furkan@Mac.mshome.net>
2025-01-02 19:46:52 +0300

    Fix hero slider CSS and database models

--- app.py
+++ app.py
@@ -80,6 +80,7 @@ class Product(db.Model):
         return f'<Product {self.name}>'
 
 class ProductImage(db.Model):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     path = db.Column(db.String(500), nullable=False)
     is_primary = db.Column(db.Boolean, default=False)
@@ -89,6 +90,7 @@ class ProductImage(db.Model):
         return f'<ProductImage {self.path}>'
 
 class Image(db.Model):
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     section = db.Column(db.String(50), nullable=False)  # hero, about
     path = db.Column(db.String(500), nullable=False)
