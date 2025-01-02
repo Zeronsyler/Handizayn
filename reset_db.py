428f4ee792390138f22c8301f8e247666926fb4f
Mehmet Furkan Karahan <furkan@Mac.mshome.net>
2025-01-02 19:23:43 +0300

    Update database reset script

--- reset_db.py
+++ reset_db.py
@@ -1,6 +1,8 @@
-from app import app, db
+# Veritabanını sıfırlamak için script oluşturuyorum
+from app import app, db, init_db
 
 with app.app_context():
     db.drop_all()
     db.create_all()
+    init_db()
     print("Veritabanı başarıyla sıfırlandı!")
